cf0860db95e444e8b850f0f8675492a1c9e2509f
Zeynep Tekin <tknzeynep3@gmail.com>
2025-08-19 08:24:31 +0300

    güncelleme

--- src/layouts/dashboard/nav.tsx
+++ src/layouts/dashboard/nav.tsx
@@ -15,7 +15,7 @@ import { RouterLink } from 'src/routes/components';
 import { Logo } from 'src/components/logo';
 import { Scrollbar } from 'src/components/scrollbar';
 
-import { NavUpgrade } from '../components/nav-upgrade';
+//import { NavUpgrade } from '../components/nav-upgrade';
 import { WorkspacesPopover } from '../components/workspaces-popover';
 
 import type { NavItem } from '../nav-config-dashboard';
@@ -188,7 +188,7 @@ export function NavContent({ data, slots, workspaces, sx }: NavContentProps) {
 
       {slots?.bottomArea}
 
-      <NavUpgrade />
+      {/* <NavUpgrade /> */}
     </>
   );
 }
